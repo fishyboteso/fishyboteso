dcbecd261bb0f87d101fe91597d6074f341a97d0
Semjon Kerner <Semjon.Kerner@gmx.net>
2021-03-28 19:31:03 +0200

    remove brackets

--- fishy/gui/main_gui.py
+++ fishy/gui/main_gui.py
@@ -132,7 +132,7 @@ def _create(gui: 'GUI'):
     # noinspection PyProtectedMember
     def set_destroy():
         if gui._bot_running:
-            if(not messagebox.askyesno(title="Quit?", message="Bot engine running. Quit Anyway?")):
+            if not messagebox.askyesno(title="Quit?", message="Bot engine running. Quit Anyway?"):
                 return
 
         config.set("win_loc", gui._root.geometry())
