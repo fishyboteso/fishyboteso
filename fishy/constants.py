a1ce1ccae925bd15bb4ca8e57401760bd83b2166
Semjon Kerner <Semjon.Kerner@gmx.net>
2021-04-12 23:12:39 +0200

    update chalutier addon for default dead color

--- fishy/constants.py
+++ fishy/constants.py
@@ -1,5 +1,5 @@
 apiversion = 2
-chalutier = ("Chalutier", "https://github.com/fishyboteso/Chalutier/raw/619b4ab0b8ff91746afda855542e886d27b7a794/Chalutier_1.1.2.zip", 112)
+chalutier = ("Chalutier", "https://github.com/fishyboteso/Chalutier/raw/main/Chalutier_1.1.3.zip", 113)
 lam2 = ("LibAddonMenu-2.0", "https://www.esoui.com/downloads/dl7/LibAddonMenu-2.0r32.zip", 32)
 
 fishyqr = ("FishyQR", "https://github.com/fishyboteso/FishyQR/files/6329586/FishyQR.zip", 100)
