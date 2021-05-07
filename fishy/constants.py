fe3715b21b2fb014f3e75b91905db7499127c0a0
Semjon Kerner <Semjon.Kerner@gmx.net>
2021-05-07 11:49:54 +0200

    fixup chalutier version

--- fishy/constants.py
+++ fishy/constants.py
@@ -1,5 +1,5 @@
 apiversion = 2
-chalutier = ("Chalutier", "https://github.com/fishyboteso/Chalutier/raw/main/Chalutier_1.1.3.zip", 113)
+chalutier = ("Chalutier", "https://www.esoui.com/downloads/dl2934/Chalutier_1.1.4.zip", 114)
 lam2 = ("LibAddonMenu-2.0", "https://www.esoui.com/downloads/dl7/LibAddonMenu-2.0r32.zip", 32)
 
 fishyqr = ("FishyQR", "https://github.com/fishyboteso/FishyQR/files/6329586/FishyQR.zip", 100)
