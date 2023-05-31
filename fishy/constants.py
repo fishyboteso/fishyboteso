5fe4c235ac8e6b029c5f809a04d713f5a2e5c084
Semjon Wilke <>
2023-05-31 22:10:47 +0200

    update addons

--- fishy/constants.py
+++ fishy/constants.py
@@ -1,14 +1,14 @@
 apiversion = 2
 
 # removed since 0.5.3
-chalutier = ("Chalutier", "https://www.esoui.com/downloads/dl2934/Chalutier_1.1.4.zip", 114)
+chalutier = ("Chalutier", "https://cdn.esoui.com/downloads/file2934/Chalutier_1.3.zip", 130)
 
 # addons used
 lam2 = ("LibAddonMenu-2.0", "https://cdn.esoui.com/downloads/file7/LibAddonMenu-2.0r34.zip", 34)
-fishyqr = ("FishyQR", "https://github.com/fishyboteso/FishyQR/releases/download/v1.6/FishyQR.zip", 151)
-libgps = ("LibGPS", "https://cdn.esoui.com/downloads/file601/LibGPS_3_2_0.zip", 32)
-libmapping = ("LibMapPing", "https://cdn.esoui.com/downloads/file1302/LibMapPing_2_0_0.zip", 200)
-libdl = ("LibDebugLogger", "https://cdn.esoui.com/downloads/file2275/LibDebugLogger_2_4_1.zip", 241)
-libchatmsg = ("LibChatMessage", "https://cdn.esoui.com/downloads/file2382/LibChatMessage_1_2_0.zip", 120)
+fishyqr = ("FishyQR", "https://github.com/fishyboteso/FishyQR/releases/download/v1.7/FishyQR_v1.7.zip", 170)
+libgps = ("LibGPS", "https://cdn.esoui.com/downloads/file601/LibGPS_v3.3.0.zip", 69)
+libmapping = ("LibMapPing", "https://cdn.esoui.com/downloads/file1302/LibMapPing_2_0_0.zip", 1236)
+libdl = ("LibDebugLogger", "https://cdn.esoui.com/downloads/file2275/LibDebugLogger_2_5_0.zip", 262)
+libchatmsg = ("LibChatMessage", "https://cdn.esoui.com/downloads/file2382/LibChatMessage_1_2_0.zip", 105)
 
 d3dshot_git = "git+https://github.com/fauskanger/D3DShot.git#egg=D3DShot"
