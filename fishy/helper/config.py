7dc9b4fe723cb0d4778736f20fd174b42be01060
mr. quacke <134191240+BDadmehr0@users.noreply.github.com>
2023-07-11 22:54:30 +0330

    Update config.py

--- fishy/helper/config.py
+++ fishy/helper/config.py
@@ -9,20 +9,20 @@ import os
 from typing import Optional
 
 from event_scheduler import EventScheduler
-# from fishy.osservices.os_services import os_services
+from fishy.osservices.os_services import os_services
 
 
-# def filename():
-#     name = "fishy_config.json"
-#     _filename = os.path.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"], "Documents", name)
-#     if os.path.exists(_filename):
-#         return _filename
+def filename():
+    name = "fishy_config.json"
+    _filename = os.path.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"], "Documents", name)
+    if os.path.exists(_filename):
+        return _filename
 
-#     # fallback for onedrive documents
-#     return os.path.join(os_services.get_documents_path(), name)
+    # fallback for onedrive documents
+    return os.path.join(os_services.get_documents_path(), name)
 
 
-# temp_file = os.path.join(os.getenv("TMPDIR", "/tmp"), "fishy_config.BAK")
+temp_file = os.path.join(os.getenv("TMPDIR", "/tmp"), "fishy_config.BAK")
 
 
 class Config:
